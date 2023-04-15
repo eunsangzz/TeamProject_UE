f9912c67ffbf0e512287691a55ca5a15fa87c29a
eunsangzz <ajffja12@gmail.com>
2023-04-15 21:41:59 +0900

    teamProject1
    2d스프라이트 적용

---
+++ TeamPjoject_UE/Source/TeamPjoject_UE/TeamPjoject_UEGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TeamPjoject_UEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAMPJOJECT_UE_API ATeamPjoject_UEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
